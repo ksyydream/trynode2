53531ea8d42d74d5125b355bce0e75d19584858f
ksyydream <249499109@qq.com>
2016-09-21 15:07:08 +0800

    2111

--- chat/socket_chat.js
+++ chat/socket_chat.js
@@ -5,21 +5,21 @@ app.use(express.static(__dirname));
 var server = app.listen(8182);*/
 
 var io = require('socket.io').listen(8182);
-console.log('server start');
+//console.log('server start');
 io.sockets.on('connection',function(socket){
-    console.log('chat start');
+    //console.log('chat start');
 
     socket.on('other event',function(data){
-        console.log(data);
+        //console.log(data);
     })
 
     socket.on('sendmsage',function(data){
-        console.log('message:'+data.message);
+        //console.log('message:'+data.message);
         socket.broadcast.emit('news',{message:data.message,user:socket.ykname});
     })
 
     socket.on('send_name',function(data){
-        console.log('收到用户昵称:'+data.name)
+        //console.log('收到用户昵称:'+data.name)
         var flag = check(data.name);
         socket.emit('back_name',{back:flag});
     })
@@ -28,7 +28,7 @@ io.sockets.on('connection',function(socket){
         var flag = check(data.name);
         if (flag == 1){
             socket.ykname = data.name;
-            console.log('成功进入聊天室:'+data.name);
+            //console.log('成功进入聊天室:'+data.name);
         }else{
             socket.emit('back_login');
         }
@@ -40,7 +40,7 @@ io.sockets.on('connection',function(socket){
 function check(name){
     for(var k in io.sockets.sockets){
         if(io.sockets.sockets[k] && io.sockets.sockets[k].ykname == name){
-            console.log('存在用户名:'+io.sockets.sockets[k].ykname)
+           // console.log('存在用户名:'+io.sockets.sockets[k].ykname)
             return 2;
         }
     }
