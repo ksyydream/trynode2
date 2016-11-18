07c5c172a793ffaec4896803c4f26a11fd1e9dcd
ksyydream <249499109@qq.com>
2016-11-18 09:56:06 +0800

    3321

---
+++ application/controllers/dclc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 16/8/1
+ * Time: 上午11:36
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//t
+class Dclc extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        ini_set('date.timezone','Asia/Shanghai');
+
+    }
+
+    //重载smarty方法assign
+    public function assign($key,$val) {
+        $this->cismarty->assign($key,$val);
+    }
+
+    //重载smarty方法display
+    public function display($html) {
+        $this->cismarty->display($html);
+    }
+
+    public function index(){
+        $this->display('dclc/dclc_info.html');
+    }
+
+}
---
+++ application/models/dclc_model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 16/6/3
+ * Time: 下午3:22
+ */
+class Dclc_model extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    /**
+     * 检查存储信息
+     *
+     * @return boolean
+     */
+    public function save_info(){
+
+    }
+}
